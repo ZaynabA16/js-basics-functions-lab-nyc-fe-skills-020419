d16e1607d94ff994820a33e727d39324b0f561a8
Zainab Agboola <zagboola@nyit.edu>
2019-03-12 00:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,12 @@
  function calculatesFarePrice(start, destination){
    if (distanceTravelledInFeet(start, destination) < 400){
      return 0; }
-   else if((distanceTravelledInFeet(start, destination)>=400 && distanceTravelledInFeet(start, destination) >2000)){
+   else if(distanceTravelledInFeet(start, destination)>=400 && distanceTravelledInFeet(start, destination) >2000){
      return (distanceTravelledInFeet - 400)* 0.02
    }
-   else if (distanceTravelledInFeet(start, destination))
+   else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination)<= 2500)
+   return 25;
+   else{
+     return 'cannot travel that far';
+   }
  }
